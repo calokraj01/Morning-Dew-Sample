f15bcabf4ffd94ace7fbee87a3f82f1931485b3a
Alok Raj <calokraj1@gmail.com>
2026-05-11 13:28:50 +0530

    commit 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Morning-Dew-Sample",
+  assetPrefix: "/Morning-Dew-Sample/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
